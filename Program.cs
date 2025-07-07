f5c1fb92ebd723d2439c55240ff4afa50790f9b3
abdul.shaikh-at-810520303341 <abdul@empezar.digital>
2025-07-07 18:09:23 +0530

    layout options added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using mudradz_layout;
+using MudBlazor;
+using MudBlazor.Services;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+ 
+builder.Services.AddMudServices();
+builder.Services.AddScoped<DialogService>(); 
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
